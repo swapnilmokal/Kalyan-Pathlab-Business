0627de9abb14f9b893a6129fbf6343ad9738aa88
swapnilmokal <swapnil.b.mokal@gmail.com>
2026-09-09 23:17:56 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='kpb-v1';
+const ASSETS=['./','./index.html','./manifest.json','./logo.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
